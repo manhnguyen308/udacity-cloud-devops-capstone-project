c6de9221f2e10c0cd3f0cbbc49cd6ea32bced048
Manh Nguyen <ndmanh.itc@gmail.com>
2023-07-18 08:18:36 +0000

    Add docker build step

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
+def page():
+    html = '''
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>My Capstone Project</title>
+        </head>
+        <body>
+            <h1>Hello, welcome to my capstone project!</h1>
+        </body>
+        </html>
+        '''
+    return html
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
